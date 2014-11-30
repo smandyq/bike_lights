b8d687d2ed2048277409712f857f6557085c518b
Smandy <smandy@quirky.com>
2014-11-29 18:14:41 -0800

    faster methods to write array.  started on fireball code

--- bike_lights.ino
+++ bike_lights.ino
@@ -37,9 +37,9 @@ typedef enum
 uint8_t ticks;
 rgb_color *led_end = &colors[0]+LED_COUNT;
 rgb_color *led_start = &colors[0];
-uint8_t sin_table[] = {5,6,7,9,11,13,16,20,24,28,34,40,48,56,65,75,87,99,113,127,141,156,171,186,200,213,225,235,244,250,254,255,254,250,244,235,225,213,200,186,171,156,141,127,113,99,87,75,65,56,48,40,34,28,24,20,16,13,11,9,7,6,5,4};
-uint8_t star_table[] = {4,16,64,255,255,232,211,192,175,159,145,132,120,110,100,91,83,75,68,62,57,52,47,43,39,35,32,29,27,24,22,20,18,17,15,14,13,11,10,10,9,8,7,7,6,5,5,4,4,4,3,3,3,3,2,2,2,2,2,1,1,1,1,1};
-uint8_t cop_table[] = {255, 0, 0, 0, 0, 0, 0, 255, 0, 0, 0, 0, 0, 0, 255, 0, 0, 0, 0, 0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+const uint8_t sin_table[] = {5,6,7,9,11,13,16,20,24,28,34,40,48,56,65,75,87,99,113,127,141,156,171,186,200,213,225,235,244,250,254,255,254,250,244,235,225,213,200,186,171,156,141,127,113,99,87,75,65,56,48,40,34,28,24,20,16,13,11,9,7,6,5,4};
+const uint8_t star_table[]  = {4,16,64,255,255,232,211,192,175,159,145,132,120,110,100,91,83,75,68,62,57,52,47,43,39,35,32,29,27,24,22,20,18,17,15,14,13,11,10,10,9,8,7,7,6,5,5,4,4,4,3,3,3,3,2,2,2,2,2,1,1,1,1,1};
+const uint8_t cop_table[] = {255, 0, 0, 0, 0, 0, 0, 255, 0, 0, 0, 0, 0, 0, 255, 0, 0, 0, 0, 0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 rgb_color rainbow[RAINBOW_MAX] = {
   {0xff, 0x00, 0x00},
   {0xff, 0x1f, 0x00},
@@ -52,9 +52,19 @@ uint8_t rando = 0; //the current index of the random number we are on.
 typedef struct 
 {
   rgb_color* pixel;
-  uint8_t*   lut_index;
+  const uint8_t*   lut_index;
   uint8_t    ticks;
 } star_t;
+
+typedef struct 
+{
+  rgb_color* start;
+  rgb_color* end;
+  rgb_color* color;
+  uint8_t value;
+  uint8_t speed;      //divisor for speed.  1, 3, 7 15, 31, 63, 127, or 255.  inversely proportional to speed.
+  uint8_t lut_increment;     //increment for the lut.  higher number decreases length.
+} fireball_t;
 
 star_t stars[LED_COUNT];
 star_t* stars_end = &stars[0] + LED_COUNT;
@@ -69,9 +79,7 @@ inline uint8_t get_rando()
 
 void update_led(rgb_color* pixel, rgb_color* color)
 {
-  pixel->red = fmax(pixel->red, color->red);
-  pixel->green = fmax(pixel->green, color->green);
-  pixel->blue = fmax(pixel->blue, color->blue);
+  memcpy(pixel, color, sizeof(rgb_color));
 }
 
 void update_stars()
@@ -140,10 +148,7 @@ void update_breathe()
 
 void update_blank()
 {
-  for(rgb_color* led = led_start; (led != led_end); led++)
-  {
-    *led = (rgb_color){ 0, 0, 0 };
-  }
+  memset(led_start, 0, sizeof(rgb_color)*LED_COUNT);
 }
 
 void setup()
@@ -153,11 +158,10 @@ void setup()
     star->lut_index=(uint8_t*)star_table_end;
     star->ticks = random(STAR_MAX_TICKS);
   }
-  for(rgb_color* led = led_start; led != led_end; led++)
-    *led = (rgb_color){ 0, 0, 0 };
+  update_blank();
 }
 
-void loop()
+void loop()  //once per "frame" of data
 {
   // Update the colors.
   //byte time = millis() >> 2;
@@ -169,5 +173,5 @@ void loop()
   // Write the colors to the LED strip.
   ledStrip.write(colors, LED_COUNT);  
   frames++;
-  //delay(10);
+//  delay(50);
 }
